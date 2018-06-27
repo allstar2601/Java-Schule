65306c8213618a4bae5fc7d40d2b6282f55ed288
allstar2601 <38939024+allstar2601@users.noreply.github.com>
2018-06-27 11:50:08 +0200

    Add files via upload

---
+++ forbsp5.java
@@ -0,0 +1,11 @@
+
+public class forbsp5 {
+	public static void main(String[] args) {
+		for (int i = 1; i <= 28; i++) {
+			for (int j = 1; j <= 45; j++) {
+				System.out.printf(" ( * ) ", i, j);
+			}
+			System.out.println();
+		}
+	}
+}
